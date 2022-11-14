ebe0db4e9ca94baae9fe4bc4b53399adcdc031df
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-11-14 13:52:00 +0000

    Create and pass withdrawal tests for logger

--- src/tests/bankLogger.test.ts
+++ src/tests/bankLogger.test.ts
@@ -6,7 +6,7 @@ describe('BankLogger', () => {
   it('should return the header string', () => {
     const account = new BankAccount();
     const logger = new BankLogger(account);
-    expect(logger.transactions).toContain('date || credit || debit || balance')
+    expect(logger.header).toContain('date || credit || debit || balance')
   })
   it('should return a list of deposits in expected format', () => {
     let currentDate = getDate();
@@ -18,5 +18,18 @@ describe('BankLogger', () => {
     expect(logger.transactions).toContain('date || credit || debit || balance');
     expect(logger.transactions).toContain(`${currentDate} || 100.00 || || 100.00`);
     expect(logger.transactions).toContain(`${currentDate} || 200.00 || || 300.00`);
+  });
+  it('should return a list of deposits and withdrawals in expected format', () => {
+    let currentDate = getDate();
+
+    const account = new BankAccount();
+    account.deposit(100);
+    account.withdraw(50);
+    account.deposit(200);
+    const logger = new BankLogger(account);
+    expect(logger.transactions).toContain('date || credit || debit || balance');
+    expect(logger.transactions).toContain(`${currentDate} || 100.00 || || 100.00`);
+    expect(logger.transactions).toContain(`${currentDate} || || 50.00 || 50.00`);
+    expect(logger.transactions).toContain(`${currentDate} || 200.00 || || 250.00`);
   });
 })
